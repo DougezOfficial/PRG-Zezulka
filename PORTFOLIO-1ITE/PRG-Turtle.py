cc29af9a4bde7a88874b23fc9d7acd21f822bb86
DougezOfficial <121669683+DougezOfficial@users.noreply.github.com>
2024-03-17 16:29:07 +0100

    Add files via upload

---
+++ PORTFOLIO-1ITE/PRG-Turtle.py
@@ -0,0 +1,15 @@
+from turtle import *
+i = 0
+while i == 0:
+    turtlesize(5)
+
+    forward(100)
+    left(120)
+    forward(200)
+    right(120)
+    forward(100)
+    right(120)
+    forward(200)
+    left(120)
+
+
