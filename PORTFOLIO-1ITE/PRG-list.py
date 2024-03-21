ee3bb5d8fcfd8d0926f5bd4d5af086728fa7ca20
DougezOfficial <121669683+DougezOfficial@users.noreply.github.com>
2024-03-21 11:32:48 +0100

    Add files via upload

---
+++ PORTFOLIO-1ITE/PRG-list.py
@@ -0,0 +1,10 @@
+ovoce = ["jablko", "hruška", "rybíz"]
+
+print(ovoce[0])
+print(ovoce[1])
+print(ovoce[2])
+
+ovoce.append("pomeranč")
+ovoce.pop(0)
+
+print(ovoce)
---
+++ PORTFOLIO-1ITE/PRG-tuple.py
@@ -0,0 +1,6 @@
+zelenina = ("mrkev", "rajče", "okurka")
+print(zelenina)
+print(len(zelenina))
+
+mix = ("auto", 34, True, 5.8)
+print(mix)
